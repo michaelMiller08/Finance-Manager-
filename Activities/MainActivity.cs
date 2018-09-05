a1b1b695de505e3ac518cb1699b000c3ea3b862b
Michael miller <michael.miller@intoware.com>
2018-09-05 09:49:50 +0100

    Solution Setup

---
+++ Activities/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace FinanceManager
+{
+    [Activity(Label = "FinanceManager", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
+
